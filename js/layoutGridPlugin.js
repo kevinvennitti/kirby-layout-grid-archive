25da1ba9b64fa05b754bfae09fbb139dbaf9f119
Kévin Vennitti <kevin.vennitti@gmail.com>
2021-11-08 00:00:07 +0100

    Now fully resizable

--- js/layoutGridPlugin.js
+++ js/layoutGridPlugin.js
@@ -1,6 +1,9 @@
 class LayoutGridPlugin {
   constructor(options) {
     this.isDragging = false;
+    this.isResizing = false;
+
+    this.resizeDirection = null;
 
     this.grid = {
       DOM: document.getElementById('layout-grid-frame'),
@@ -27,7 +30,7 @@ class LayoutGridPlugin {
       }
     };
 
-    this.itemDragged = {
+    this.item = {
       DOM: null,
       origin: {
         x: null,
@@ -39,6 +42,8 @@ class LayoutGridPlugin {
       rowSpan: null
     };
 
+    this.itemBeforeTransformations = null;
+
     this.init(options);
   }
 
@@ -46,7 +51,8 @@ class LayoutGridPlugin {
     const defaults = {
       itemSelector: '.layout-grid-item',
       cols: 12,
-      rows: 12
+      rows: 12,
+      gap: 16
     };
 
     let params = Object.assign({}, defaults, options);
@@ -56,9 +62,10 @@ class LayoutGridPlugin {
 
     this.grid.nbCols = params.cols;
     this.grid.nbRows = params.rows;
+    this.grid.gap = params.gap;
 
     this.getAllLimits();
-    this.initLayoutGridItems();
+    this.initEvents();
   }
 
   getAllLimits() {
@@ -110,56 +117,143 @@ class LayoutGridPlugin {
 
     this.grid.rowsStartsY.splice(-1, 1);
     this.grid.rowsEndsY.shift();
+
+    console.log(this.grid);
   }
 
-  triggerMouseDown(item, e) {
+  triggerDragItem(item, e) {
     this.setMouseOrigin(e);
     this.updateMouse(e);
 
+    this.setItem(item);
     this.setDragOn();
-    this.setItemDragged(item);
   }
 
   triggerMouseMove(e) {
-    var _this = this;
+    this.updateMouse(e);
 
     if (this.isDragging) {
-      this.updateMouse(e);
-
-      let dragPositionX = this.itemDragged.origin.x - this.mouse.delta.x;
-      let dragPositionY = this.itemDragged.origin.y - this.mouse.delta.y;
-
-      // check x position
-      this.grid.colsStartsX.forEach(function(colStartX, i) {
-        if (i <= _this.grid.nbCols - _this.itemDragged.columnSpan) {
-          if (dragPositionX >= colStartX - 2
-           && dragPositionX < _this.grid.colsStartsX[i+1] - 2) { // tolerance: 2 pixels
-            _this.itemDragged.columnStart = i + 1;
-            _this.itemDragged.DOM.style.gridColumnStart = _this.itemDragged.columnStart;
-          }
-        }
-      });
-
-      // check y position
-      this.grid.rowsStartsY.forEach(function(rowStartY, i) {
-        if (dragPositionY >= rowStartY - 2
-         && dragPositionY < _this.grid.rowsStartsY[i+1] - 2) { // tolerance: 2 pixels
-          _this.itemDragged.rowStart = i + 1;
-          _this.itemDragged.DOM.style.gridRowStart = _this.itemDragged.rowStart;
-        }
-      });
-
-      this.itemDragged.columnStart = parseInt(this.getStyleProperty(this.itemDragged.DOM, 'grid-column-start'));
-      this.itemDragged.columnSpan = parseInt(this.getStyleProperty(this.itemDragged.DOM, 'grid-column-end').replace('span ',''));
-      this.itemDragged.rowStart = parseInt(this.getStyleProperty(this.itemDragged.DOM, 'grid-row-start'));
-      this.itemDragged.rowSpan = parseInt(this.getStyleProperty(this.itemDragged.DOM, 'grid-row-end').replace('span ',''));
+      this.checkDrag();
     }
+
+    if (this.isResizing) {
+      this.checkResize();
+    }
+  }
+
+  checkDrag() {
+    var _this = this;
+
+    this.getItemStyles();
+
+    let dragPositionX = this.item.origin.x - this.mouse.delta.x;
+    let dragPositionY = this.item.origin.y - this.mouse.delta.y;
+
+    // check x position
+    this.grid.colsStartsX.forEach(function(colStartX, i) {
+      if (i <= _this.grid.nbCols - _this.item.columnSpan) {
+        if (dragPositionX >= colStartX - 2
+         && dragPositionX < _this.grid.colsStartsX[i+1] - 2) { // tolerance: 2 pixels
+          _this.item.columnStart = i + 1;
+          _this.item.DOM.style.gridColumnStart = _this.item.columnStart;
+        }
+      }
+    });
+
+    // check y position
+    this.grid.rowsStartsY.forEach(function(rowStartY, i) {
+      if (dragPositionY >= rowStartY - 2
+       && dragPositionY < _this.grid.rowsStartsY[i+1] - 2) { // tolerance: 2 pixels
+        _this.item.rowStart = i + 1;
+        _this.item.DOM.style.gridRowStart = _this.item.rowStart;
+      }
+    });
+
+    this.getItemStyles();
+  }
+
+  checkResize() {
+    var _this = this;
+
+    this.getItemStyles();
+
+    let dragPositionX = this.item.origin.x - this.mouse.delta.x;
+    let dragPositionY = this.item.origin.y - this.mouse.delta.y;
+
+    if (this.resizeDirection == 'e') {
+      // check x position to east
+      this.grid.colsEndsX.every(function(colEndX, i) {
+        if (_this.mouse.current.x < (colEndX + _this.grid.gap)
+          && i + 2 > _this.item.columnStart) {
+          _this.item.columnSpan = i + 2 - _this.item.columnStart;
+          _this.item.DOM.style.gridColumnEnd = 'span ' + _this.item.columnSpan;
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+
+    if (this.resizeDirection == 'w') {
+      // check x position to west
+      this.grid.colsEndsX.every(function(colEndX, i) {
+        if (_this.mouse.current.x < colEndX) {
+          _this.item.columnStart = Math.min(i + 1, _this.itemBeforeTransformations.columnSpan + _this.itemBeforeTransformations.columnStart - 1);
+          _this.item.DOM.style.gridColumnStart = _this.item.columnStart;
+
+          _this.item.columnSpan = _this.itemBeforeTransformations.columnSpan + (_this.itemBeforeTransformations.columnStart - _this.item.columnStart);
+          _this.item.DOM.style.gridColumnEnd = 'span ' + _this.item.columnSpan;
+
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+
+    if (this.resizeDirection == 's') {
+      // check y position to south
+      this.grid.rowsEndsY.every(function(rowEndY, i) {
+        if (_this.mouse.current.y <= rowEndY) {
+          _this.item.rowSpan = i + 1 - _this.item.rowStart;
+          _this.item.DOM.style.gridRowEnd = 'span ' + _this.item.rowSpan;
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+
+    if (this.resizeDirection == 'n') {
+      // check x position to west
+      this.grid.rowsEndsY.every(function(rowEndY, i) {
+        if (_this.mouse.current.y < rowEndY) {
+          _this.item.rowStart = Math.min(i + 1, _this.itemBeforeTransformations.rowSpan + _this.itemBeforeTransformations.rowStart - 1);
+          _this.item.DOM.style.gridRowStart = _this.item.rowStart;
+
+          _this.item.rowSpan = _this.itemBeforeTransformations.rowSpan + (_this.itemBeforeTransformations.rowStart - _this.item.rowStart);
+          _this.item.DOM.style.gridRowEnd = 'span ' + _this.item.rowSpan;
+
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+
+    this.getItemStyles();
+
+    //this.item.columnSpan = parseInt(this.getStyleProperty(this.item.DOM, 'grid-column-end').replace('span ',''));
+    //this.item.rowSpan = parseInt(this.getStyleProperty(this.item.DOM, 'grid-row-end').replace('span ',''));
   }
 
   triggerMouseUp(e) {
     if (this.isDragging) {
-      this.resetMouse();
       this.setDragOff();
+    }
+
+    if (this.isResizing) {
+      this.setResizeOff();
     }
   }
 
@@ -169,31 +263,54 @@ class LayoutGridPlugin {
 
   setDragOff() {
     this.resetMouse();
-    this.unsetItemDragged();
+    this.unsetItem();
     this.isDragging = false;
   }
 
-  setItemDragged(item) {
-    this.itemDragged.DOM = item;
-    this.itemDragged.origin.x = this.itemDragged.DOM.getBoundingClientRect().left - this.grid.DOM.getBoundingClientRect().left;
-    this.itemDragged.origin.y = this.itemDragged.DOM.getBoundingClientRect().top - this.grid.DOM.getBoundingClientRect().top;
+  setResizeOn(resizeDirection) {
+    this.isResizing = true;
+    this.resizeDirection = resizeDirection;
+    this.itemBeforeTransformations = JSON.parse(JSON.stringify(this.item));
+    console.log(this.itemBeforeTransformations);
   }
 
-  unsetItemDragged() {
-    this.itemDragged.DOM = null;
+  setResizeOff() {
+    this.resetMouse();
+    this.unsetItem();
+    this.isResizing = false;
+    this.resizeDirection = null;
+  }
+
+  setItem(item) {
+    this.item.DOM = item;
+    this.item.origin.x = this.item.DOM.getBoundingClientRect().left - this.grid.DOM.getBoundingClientRect().left;
+    this.item.origin.y = this.item.DOM.getBoundingClientRect().top - this.grid.DOM.getBoundingClientRect().top;
+
+    this.getItemStyles();
+  }
+
+  getItemStyles() {
+    this.item.columnStart = parseInt(this.getStyleProperty(this.item.DOM, 'grid-column-start'));
+    this.item.columnSpan = parseInt(this.getStyleProperty(this.item.DOM, 'grid-column-end').replace('span ',''));
+    this.item.rowStart = parseInt(this.getStyleProperty(this.item.DOM, 'grid-row-start'));
+    this.item.rowSpan = parseInt(this.getStyleProperty(this.item.DOM, 'grid-row-end').replace('span ',''));
+  }
+
+  unsetItem() {
+    this.item.DOM = null;
   }
 
   updateMouse(e) {
-    this.mouse.current.x = e.pageX;
-    this.mouse.current.y = e.pageY;
+    this.mouse.current.x = e.pageX - this.grid.DOM.getBoundingClientRect().left;
+    this.mouse.current.y = e.pageY - this.grid.DOM.getBoundingClientRect().top;
 
     this.mouse.delta.x = this.mouse.origin.x - this.mouse.current.x;
     this.mouse.delta.y = this.mouse.origin.y - this.mouse.current.y;
   }
 
   setMouseOrigin(e) {
-    this.mouse.origin.x = e.pageX;
-    this.mouse.origin.y = e.pageY;
+    this.mouse.origin.x = e.pageX - this.grid.DOM.getBoundingClientRect().left;
+    this.mouse.origin.y = e.pageY - this.grid.DOM.getBoundingClientRect().top;
   }
 
   resetMouse() {
@@ -205,25 +322,45 @@ class LayoutGridPlugin {
     this.mouse.delta.y = null;
   }
 
+  triggerResizeItem(item, e, resizeDirection) {
+    this.setMouseOrigin(e);
+    this.updateMouse(e);
+
+    this.setItem(item);
+    this.setResizeOn(resizeDirection);
+  }
+
   getStyleProperty(dom, property) {
     return window.getComputedStyle(dom, null).getPropertyValue(property);
   }
 
-  initLayoutGridItems() {
+  initEvents() {
+    var _this = this;
+
     let layoutGridItems = document.querySelectorAll(this.params.itemSelector);
 
     layoutGridItems.forEach(layoutGridItem => {
-      layoutGridItem.addEventListener('mousedown', function(e) {
-        layoutGridPlugin.triggerMouseDown(this, e);
+      var _layoutGridItem = layoutGridItem;
+
+      layoutGridItem.querySelectorAll('.layout-grid-item-drag-handle').forEach(layoutGridItemDragHandle => {
+        layoutGridItemDragHandle.addEventListener('mousedown', function(e) {
+          _this.triggerDragItem(_layoutGridItem, e);
+        });
+      });
+
+      layoutGridItem.querySelectorAll('.layout-grid-item-resize-handle').forEach(layoutGridItemDragHandle => {
+        layoutGridItemDragHandle.addEventListener('mousedown', function(e) {
+          _this.triggerResizeItem(_layoutGridItem, e, this.getAttribute('data-resize'));
+        });
       });
     });
 
     document.addEventListener('mousemove', function(e){
-      layoutGridPlugin.triggerMouseMove(e);
+      _this.triggerMouseMove(e);
     });
 
     document.addEventListener('mouseup', function(e){
-      layoutGridPlugin.triggerMouseUp(e);
+      _this.triggerMouseUp(e);
     });
   }
 }
